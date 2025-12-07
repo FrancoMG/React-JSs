25245461cf4e55a11020a27a7be26d69b925ad51
Franco Molina González <molinagonzalezfranco@gmail.com>
2025-12-06 23:04:33 -0300

    Código final corregido para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Reemplaza esto con el nombre de tu repositorio
-  base: '/React-JSs/', 
 })
